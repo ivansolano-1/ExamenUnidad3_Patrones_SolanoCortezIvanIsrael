558b16ccad63c71c317add18f872ef571ae42c3c
ivan dosca <l22210786@tectijuana.edu.mx>
2025-11-13 02:04:34 -0800

    Add EstudianteBase class with basic properties

--- SistemaEducativo/ClasesBase/EstudianteBase.cs
+++ SistemaEducativo/ClasesBase/EstudianteBase.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace SistemaEducativo
+{
+    public class EstudianteBase : IEstudiante
+    {
+        private string nombre;
+        private double calificacion;
+
+        public EstudianteBase(string nombre, double calificacion)
+        {
+            this.nombre = nombre;
+            this.calificacion = calificacion;
+        }
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Estudiante: {nombre}");
+            Console.WriteLine($"Calificación base: {calificacion}");
+        }
+        public double ObtenerCalificacionBase()
+        {
+            return calificacion;
+        }
+    }
+}
